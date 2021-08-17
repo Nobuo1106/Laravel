16dd56630091aa005c11f50e9c0bf324997232bb
Nobuo1106 <gonnbe1986@gmail.com>
2021-08-17 22:17:59 +0900

    コントローラー作成

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index($id ='noname', $pass='unknown') {
+        return <<< EOF
+<html>
+<head>
+<title>Hello/Index</title>
+<style>
+body {font-size16pt; color:#999; }
+h1 { font-size:16pt; text-align:right; color:#eee;
+    margin:-40px 0px -50px 0px; }
+</style>
+<body>
+    <h1>Index</h1>
+    <p>これはHelloコントローラのIndexアクションです。</p>
+    <li>ID: {$id}</li>
+    <li>PASS: {$pass}</li>
+</body>
+</html>
+EOF;
+    }
+}
